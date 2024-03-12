8e30e4851368eb2f628882c9144f7790186033de
eacanales <eacanales@outlook.com>
2024-03-11 22:10:20 -0400

    message

--- calculateGrocery/grocery.js
+++ calculateGrocery/grocery.js
@@ -5,9 +5,9 @@ let grocery3;
 function groceryTracker() {
     grocery1 = parseFloat(document.getElementById('grocery1').value);
     grocery2 = parseFloat(document.getElementById('grocery2').value);
-    grocery2 = parseFloat(document.getElementById('grocery3').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
    
     let total = grocery1 + grocery2 + grocery3;
 
-    document.getElementById('result').innerText = `The total amount is: ${total}`;
+    document.getElementById('result').innerText = `The total amount is:$ ${total}`;
 }
