af05e943b575d6521204cab3f89abcd4c7184400
eacanales <eacanales@outlook.com>
2024-04-03 10:26:01 -0400

    message

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -36,9 +36,11 @@ function startTest() {
                     wpm = Math.round((typedWords / timeElapsed) * 60);
                 }
     
+
                 // Display the results
                 var outputDiv = document.getElementById("output");
                 outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                    "<p>Total Length: " + userTypedText.length + "</p>" +
                     "<p>Words Typed: " + typedWords + "</p>" +
                     "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                     "<p>Words Per Minute (WPM): " + wpm + "</p>";
